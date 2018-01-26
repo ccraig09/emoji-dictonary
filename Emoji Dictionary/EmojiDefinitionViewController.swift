d3c7339ceca011810d3f20d6c2ba3b5fd2eee1d4
Carlos Craig <carlos.craig09@gmail.com>
2018-01-26 08:44:30 -0400

    we have version 1.0!
    i did an emoji app!

--- Emoji Dictionary/EmojiDefinitionViewController.swift
+++ Emoji Dictionary/EmojiDefinitionViewController.swift
@@ -13,11 +13,26 @@ class EmojiDefinitionViewController: UIViewController {
     @IBOutlet weak var emojilabel: UILabel!
     var emoji = ""
 
+    @IBOutlet weak var emojiDefinitionLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
      emojilabel.text = emoji
+        
+        // ["😀","😊","😇","🙂"]//
+        
+        if emoji == "😀" {
+            emojiDefinitionLabel.text! = "Happy Smiley Face"
+        }
+        
+        if emoji == "😊" {
+            emojiDefinitionLabel.text! = "A Normal Happy Smiley Face"
+        }
+        if emoji == "😇" {
+            emojiDefinitionLabel.text = "An Angel Smiley Face"
+        }
     }
+    
 
    
 
